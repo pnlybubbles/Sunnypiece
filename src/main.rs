a4d1620d840db50c2d7106832b90c05efdb09469
pnly <pnlybubbles@gmail.com>
2020-05-16 15:27:06 +0900

    コメントを日本語にした

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,13 @@ const SPP: usize = 1;
 type Image = PPM;
 
 fn main() {
-  // Film
+  // フィルム
   let mut film = Film::new(Vector3::zero(), WIDTH, HEIGHT);
-  // Camera
+
+  // カメラ
   let camera = IdealPinhole::new(PI / 2.0, film.aspect(), Matrix4::unit());
-  // Scene
+
+  // シーン
   let sphere = Object::new(
     Box::new(Sphere {
       position: Vector3::new(0.0, 0.0, -5.0),
@@ -40,18 +42,21 @@ fn main() {
     Matrix4::unit(),
   );
   let objects = vec![sphere];
+
+  // 空間構造
   let structure = acceleration::Linear::new(objects);
+
   let scene = Scene {
     camera: camera,
     structure: structure,
   };
 
   {
-    // Integrator
+    // 積分器
     let mut integrator = DebugIntegrator::new(&mut film);
 
     integrator.each(|apply, u, v| {
-      // Light transport
+      // 光輸送
       debug_assert!(u.less_than_unit(), "0 <= u < 1.0");
       debug_assert!(v.less_than_unit(), "0 <= v < 1.0");
       let ray = scene.camera.sample(u, v);
@@ -67,14 +72,13 @@ fn main() {
     });
   }
 
-  // Path
+  // 保存
   let file_path = &format!(
     "images/image_{}_{}.{}",
     time::now().strftime("%Y%m%d%H%M%S").unwrap(),
     SPP,
     Image::ext(),
   );
-  // Save
   Image::save(&film, Path::new(&file_path), |v| {
     let correct = v.map(|v| v.min(1.0).max(0.0) * 255.0);
     [correct.x as u8, correct.y as u8, correct.z as u8]
