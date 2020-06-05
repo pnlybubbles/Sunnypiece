ca68781e9cf9c841a55f88aab3d394284fdd1f5e
pnly <pnlybubbles@gmail.com>
2020-06-06 01:49:06 +0900

    Implements reducing catastrophic cancellation of quadratic solutions

--- src/geometry/sphere.rs
+++ src/geometry/sphere.rs
@@ -29,21 +29,27 @@ impl Geometry for Sphere {
     let r2 = self.radius * self.radius;
     // 判別式 Δ = b^2 - a*c = (d.f)^2 - (f^2 - r^2)
     // f^2 - r^2 で半径よりもはるか遠い点からの衝突の場合に情報落ちが起こる
+    // Hearn and Baker equation
     // 変形 Δ = r^2 - (f - d(d.f))^2
     let det = r2 - (f - d * b).sqr_norm();
     // 交差しない
     if det < 0.0 {
       return None;
     }
-    let t1 = -b - det.sqrt();
-    let t2 = -b + det.sqrt();
+    // b ~ √Δ の場合に桁落ちが起こる
+    // 桁落ちが起こらない方の解を使って導出する
+    let c = f.sqr_norm() - r2;
+    let q = -b - b.signum() * det.sqrt();
+    let t1 = q;
+    let t2 = c / q;
+    let (tl, tg) = if t1 < t2 { (t1, t2) } else { (t2, t1) };
     // 出射方向と反対側で交差
-    if t2 < EPS {
+    if tg < EPS {
       return None;
     }
     // 近い方が正の場合はそれを採用
     // それ以外(球体内部からの交差の場合)は正の方を採用
-    let distance = if t1 > EPS { t1 } else { t2 };
+    let distance = if tl > EPS { tl } else { tg };
     // r = o + t * d
     let position = ray.origin + ray.direction * distance;
     // 法線は球体中心から外向き
