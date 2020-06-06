19adf120d5aafe3aa7d2cc8d3b9677c87e2d012c
pnly <pnlybubbles@gmail.com>
2020-06-06 23:07:30 +0900

    No mut

--- src/math/basis.rs
+++ src/math/basis.rs
@@ -10,12 +10,12 @@ impl OrthonormalBasis for Vector3 {
     // Gram Schmidtの正規直交化法
     // If n is near the x-axis , use the y- axis . Otherwise use the x- axis .
     let n = *self;
-    let mut b1 = if n.x > 0.9 {
+    let b0 = if n.x > 0.9 {
       Vector3::new(0.0, 1.0, 0.0)
     } else {
       Vector3::new(1.0, 0.0, 0.0)
     };
-    b1 = (b1 - n * b1.dot(n)).normalize(); // Make b1 orthogonal to n
+    let b1 = (b0 - n * b0.dot(n)).normalize(); // Make b1 orthogonal to n
     let b2 = n.cross(b1); // Construct b2 using a cross product
     [b1, b2, n].into()
   }
