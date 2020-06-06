3f0e78ee4dbf00e71a3a35f83a40185b68bda775
pnly <pnlybubbles@gmail.com>
2020-06-06 23:01:18 +0900

    Rename

--- src/light_transport/explicit_light.rs
+++ src/light_transport/explicit_light.rs
@@ -42,37 +42,38 @@ where
     // マテリアルに基づいて方向ベクトルをサンプリング
     let material_sample = point.sample_material();
     // 衝突点から方向ベクトルを使ってパスを接続
-    let material_throughput = match point.connect_direction(self.structure, material_sample.value) {
-      None => Vector3::zero(),
-      Some(geom) => {
-        // マテリアルに比例した光源サブパスの重点的サンプリング
-        let li = geom.next.emittance();
-        let light_pdf = geom.light_pdf(&self.light_sampler);
-        debug_assert!(light_pdf.map(|v| v.0).unwrap_or(0.0).is_finite());
-        let bsdf_pdf = geom.bsdf_pdf();
-        debug_assert!(bsdf_pdf.0.is_finite());
-        let light_contrib = light_pdf
-          .map(|pdf| {
-            let mis_weight = bsdf_pdf.power_hulistic(pdf, 2);
-            debug_assert!(mis_weight.is_finite());
-            debug_assert!(geom.bsdf().is_finite());
-            debug_assert!(geom.weight(bsdf_pdf).is_finite());
-            li * geom.bsdf() * geom.weight(bsdf_pdf) * mis_weight
-          })
-          .unwrap_or(Vector3::zero());
-        debug_assert!(light_contrib.is_finite());
-        // 接続先から再帰的にパスを生成して散乱成分の寄与を蓄積する
-        let li_scatter = self.radiance_recursive(&geom.next, depth + 1);
-        let scatter_contrib = li_scatter * geom.bsdf() * geom.weight(material_sample.pdf);
-        debug_assert!(scatter_contrib.is_finite());
-        light_contrib + scatter_contrib
-      }
-    };
+    let material_oriented_contrib =
+      match point.connect_direction(self.structure, material_sample.value) {
+        None => Vector3::zero(),
+        Some(geom) => {
+          // マテリアルに比例した光源サブパスの重点的サンプリング
+          let li = geom.next.emittance();
+          let light_pdf = geom.light_pdf(&self.light_sampler);
+          debug_assert!(light_pdf.map(|v| v.0).unwrap_or(0.0).is_finite());
+          let bsdf_pdf = geom.bsdf_pdf();
+          debug_assert!(bsdf_pdf.0.is_finite());
+          let light_contrib = light_pdf
+            .map(|pdf| {
+              let mis_weight = bsdf_pdf.power_hulistic(pdf, 2);
+              debug_assert!(mis_weight.is_finite());
+              debug_assert!(geom.bsdf().is_finite());
+              debug_assert!(geom.weight(bsdf_pdf).is_finite());
+              li * geom.bsdf() * geom.weight(bsdf_pdf) * mis_weight
+            })
+            .unwrap_or(Vector3::zero());
+          debug_assert!(light_contrib.is_finite());
+          // 接続先から再帰的にパスを生成して散乱成分の寄与を蓄積する
+          let li_scatter = self.radiance_recursive(&geom.next, depth + 1);
+          let scatter_contrib = li_scatter * geom.bsdf() * geom.weight(material_sample.pdf);
+          debug_assert!(scatter_contrib.is_finite());
+          light_contrib + scatter_contrib
+        }
+      };
 
     // 明示的に光源の座標をサンプリング
     let light_sample = self.light_sampler.sample();
     // 衝突点と光源を接続して光源サブパスを生成
-    let light_throughput = match point.connect_point(self.structure, light_sample.value) {
+    let light_oriented_contrib = match point.connect_point(self.structure, light_sample.value) {
       None => Vector3::zero(),
       Some(geom) => {
         // 明示的な光源サブパスの重点的サンプリング
@@ -87,7 +88,7 @@ where
       }
     };
 
-    le + material_throughput + light_throughput
+    le + material_oriented_contrib + light_oriented_contrib
   }
 }
 
