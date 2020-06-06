e69ca945247b2adb29170a6555e16bfa2d6ed110
pnly <pnlybubbles@gmail.com>
2020-06-06 21:49:25 +0900

    Add intersection test utility

---
+++ src/light_transport/intersection_test.rs
@@ -0,0 +1,58 @@
+use super::radiance::Radiance;
+use acceleration::Acceleration;
+use math::*;
+use ray::Ray;
+use sampler::Sampler;
+
+pub struct IntersectionTest<'a, S>
+where
+  S: Acceleration,
+{
+  structure: &'a S,
+}
+
+impl<'a, S> IntersectionTest<'a, S>
+where
+  S: Acceleration + 'a,
+{
+  pub fn new(structure: &'a S) -> Self {
+    IntersectionTest {
+      structure: structure,
+    }
+  }
+}
+
+impl<'a, S> Radiance for IntersectionTest<'a, S>
+where
+  S: Acceleration,
+{
+  fn radiance(&self, ray: Ray) -> Vector3 {
+    let maybe_interaction = self.structure.interact(ray);
+
+    match maybe_interaction {
+      None => Vector3::zero(),
+      Some(point) => {
+        for _ in 0..1000 {
+          let sample = Sampler::hemisphere_uniform();
+          let basis = point.normal().orthonormal_basis();
+          let direction = &basis * sample;
+          match point.connect_direction(self.structure, direction) {
+            None => (),
+            Some(geom) => {
+              if geom.current.orienting_normal.dot(geom.wi) > EPS {
+                println!(
+                  "{} {} {}",
+                  geom.current.orienting_normal.dot(geom.wi),
+                  geom.next.orienting_normal.dot(-geom.wi),
+                  geom.path_i.norm(),
+                );
+                return Vector3::new(1.0, 0.0, 0.0);
+              }
+            }
+          }
+        }
+        return Vector3::new(0.0, 1.0, 0.0);
+      }
+    }
+  }
+}
--- src/light_transport/mod.rs
+++ src/light_transport/mod.rs
@@ -1,9 +1,11 @@
 mod explicit_light;
+mod intersection_test;
 mod naive;
 mod normal;
 mod radiance;
 
 pub use self::explicit_light::*;
+pub use self::intersection_test::*;
 pub use self::naive::*;
 pub use self::normal::*;
 pub use self::radiance::*;
