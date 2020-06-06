0895fa7d53cbaad2fd818f464f3f8168fb8a7a62
pnly <pnlybubbles@gmail.com>
2020-06-06 22:29:51 +0900

    Use Gram Schmidt algorithm

--- src/math/basis.rs
+++ src/math/basis.rs
@@ -1,4 +1,4 @@
-use super::{Cross, Matrix4, Normalize, Vector3, EPS};
+use super::*;
 
 pub trait OrthonormalBasis: Sized {
   fn orthonormal_basis(&self) -> Matrix4;
@@ -7,14 +7,16 @@ pub trait OrthonormalBasis: Sized {
 impl OrthonormalBasis for Vector3 {
   // 自身(normal)を基準として正規直交基底を生成 (正規化済み前提)
   fn orthonormal_basis(&self) -> Matrix4 {
-    let tangent = if self.x.abs() > EPS {
+    // Gram Schmidtの正規直交化法
+    // If n is near the x-axis , use the y- axis . Otherwise use the x- axis .
+    let n = *self;
+    let mut b1 = if n.x > 0.9 {
       Vector3::new(0.0, 1.0, 0.0)
     } else {
       Vector3::new(1.0, 0.0, 0.0)
-    }
-    .cross(*self)
-    .normalize();
-    let bionrmal = self.cross(tangent);
-    [tangent, bionrmal, *self].into()
+    };
+    b1 = (b1 - n * b1.dot(n)).normalize(); // Make b1 orthogonal to n
+    let b2 = n.cross(b1); // Construct b2 using a cross product
+    [b1, b2, n].into()
   }
 }
