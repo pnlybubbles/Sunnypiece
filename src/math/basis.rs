62dff0b9aa47e620f651538a6dc82d92da0c7813
pnly <pnlybubbles@gmail.com>
2020-06-13 22:29:57 +0900

    グランシュミットの直交化がミスってた

--- src/math/basis.rs
+++ src/math/basis.rs
@@ -6,16 +6,17 @@ pub trait OrthonormalBasis: Sized {
 
 impl OrthonormalBasis for Vector3 {
   // 自身(normal)を基準として正規直交基底を生成 (正規化済み前提)
+  // Gram Schmidtの正規直交化法
   fn orthonormal_basis(&self) -> Matrix4 {
-    // Gram Schmidtの正規直交化法
     // If n is near the x-axis , use the y- axis . Otherwise use the x- axis .
     let n = *self;
-    let b0 = if n.x > 0.9 {
+    let mut b1 = if n.x.abs() > 0.9 {
       Vector3::new(0.0, 1.0, 0.0)
     } else {
       Vector3::new(1.0, 0.0, 0.0)
     };
-    let b1 = (b0 - n * b0.dot(n)).normalize(); // Make b1 orthogonal to n
+    b1 -= n * b1.dot(n); // Make b1 orthogonal to n
+    b1 *= 1.0 / b1.dot(b1).sqrt(); // Normalize b1
     let b2 = n.cross(b1); // Construct b2 using a cross product
     [b1, b2, n].into()
   }
