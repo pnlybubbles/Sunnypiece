a36c3353543c5b042283f591cdb49deafc0e1764
pnly <pnlybubbles@gmail.com>
2020-06-18 02:06:18 +0900

    新しいプログレスインジケータを作った

--- src/integrator/util.rs
+++ src/integrator/util.rs
@@ -12,3 +12,49 @@ pub fn progress_indicator(progress: usize, total: usize) {
   )
   .unwrap();
 }
+
+pub struct ProgressIndicator {
+  total: usize,
+  progress: usize,
+  start_time: time::Tm,
+}
+
+impl ProgressIndicator {
+  pub fn new(total: usize) -> Self {
+    let start_time = time::now();
+    println!("start: {}", start_time.strftime("%+").unwrap());
+
+    ProgressIndicator {
+      total: total,
+      progress: 0,
+      start_time: start_time,
+    }
+  }
+
+  pub fn next(&mut self) {
+    self.progress += 1;
+    self.update()
+  }
+
+  pub fn end(&self) {
+    let end_time = time::now();
+    println!("");
+    println!("end: {}", end_time.strftime("%+").unwrap());
+    println!(
+      "elapse: {}s",
+      (end_time - self.start_time).num_milliseconds() as f32 / 1000.0
+    );
+  }
+
+  fn update(&self) {
+    let stdout = io::stdout();
+    write!(
+      &mut stdout.lock(),
+      "\rprocessing... ({}/{} : {:.0}%) ",
+      self.progress,
+      self.total,
+      self.progress as f32 / self.total as f32 * 100.0
+    )
+    .unwrap();
+  }
+}
